3fa0ee35c61baf6630771bcfa50a9299640dd26a
Reginaldo Leobino Junior <reginaldo.leobino@fatec.sp.gov.br>
2021-07-19 15:53:54 -0300

    Finalizando Core de exemplo com controller

--- app/Core/Core.php
+++ app/Core/Core.php
@@ -3,8 +3,11 @@
     class Core{
         public function start($urlGet)
         {
-            if(isset($urlGet['pagina'])){          
+            if(isset($urlGet['pagina']) and $urlGet['pagina'] == 'sobre'){          
                 $controller = ucfirst($urlGet['pagina'].'Controller');
+                echo ' Exemplo de controle do tipo sobre<br>';
+            } else if($urlGet['pagina'] == 'servico'){
+                echo '  Exemplo de controle do tipo servico<br>';
             } else {
                 $controller = 'HomeController';
             }
@@ -14,12 +17,11 @@
             if(!class_exists($controller))
             {
                 $controller = 'ErroController';
+                var_dump($urlGet);
             }
 
             // callback para criar um objeto, chamando um método.
             call_user_func_array(array(new $controller, $acao), array());
-
-
 
         }
     }
